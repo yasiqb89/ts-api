1be16215c4f42d4e399b228cdb1f752de205f656
yasiqb89 <mian.yassir@gmail.com>
2026-01-22 16:06:14 +0100

    Auth bug fixes.

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -3,11 +3,6 @@ import { z } from "zod";
 export const LoginResponseSchema = z.object({
     id: z.number(),
     username: z.string(),
-    email: z.string().optional(),
-    firstName: z.string().optional(),
-    lastName: z.string().optional(),
-    gender: z.string().optional(),
-    image: z.string().optional(),
     accessToken: z.string(),
     refreshToken: z.string(),
 });
@@ -15,11 +10,6 @@ export const LoginResponseSchema = z.object({
 export const AuthMeSchema = z.object({
     id: z.number(),
     username: z.string(),
-    email: z.string().optional(),
-    firstName: z.string().optional(),
-    lastName: z.string().optional(),
-    gender: z.string().optional(),
-    image: z.string().optional(),
 });
 
 export const RefreshResponseSchema = z.object({
